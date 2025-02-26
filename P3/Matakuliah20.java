078f5976ed49aea781c654761d8d7c078e8f5307
daebon07 <muhammadshabran25@gmail.com>
2025-02-26 11:46:03 +0700

    P3_Percobaan3_Pertanyaan2

--- P3/Matakuliah20.java
+++ P3/Matakuliah20.java
@@ -1,16 +1,33 @@
 package P3;
-
+import java.util.Scanner;
 public class Matakuliah20 {
     public String kode;
     public String nama;
     public int sks;
     public int jumlahjam;
 
+  public Matakuliah20() {
+   
+        }
     public Matakuliah20(String kode, String nama,int sks,int jumlahjam){
         this.kode= kode;
         this.nama= nama;
         this.sks= sks;
         this.jumlahjam=jumlahjam;
+    }
+    
+    public void tambahData() {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Kode        : ");
+        this.kode = sc.nextLine();
+        System.out.print("Nama        : ");
+        this.nama = sc.nextLine();
+        System.out.print("Sks         : ");
+        this.sks = Integer.parseInt(sc.nextLine());
+        System.out.print("Jumlah Jam  : ");
+        this.jumlahjam = Integer.parseInt(sc.nextLine());
+        System.out.println("------------------------------");
     }
     
 }
--- P3/Matakuliahdemo20.java
+++ P3/Matakuliahdemo20.java
@@ -9,22 +9,12 @@ public class Matakuliahdemo20 {
         String kode, nama, dummy;
         int sks, jumlahJam;
 
-        for(int i=0; i < 3; i++) {
+        for (int i = 0; i < 3; i++) {
             System.out.println("Masukkan Data Matakuliah ke-" + (i + 1));
-            System.out.print("Kode        : ");
-            kode = sc.nextLine();
-            System.out.print("Nama        : ");
-            nama = sc.nextLine();
-            System.out.print("Sks         : ");
-            dummy = sc.nextLine();
-            sks = Integer.parseInt(dummy);
-            System.out.print("Jumlah Jam  : ");
-            dummy = sc.nextLine();
-            jumlahJam = Integer.parseInt(dummy);
-            System.out.println("------------------------------");
-
-            arrayOfMatakuliah20[i] = new Matakuliah20(kode, nama, sks, jumlahJam);
+            arrayOfMatakuliah20[i] = new Matakuliah20();
+            arrayOfMatakuliah20[i].tambahData();
         }
+
         for(int i=0; i < 3; i++) {
             System.out.println("Data Matakuliah ke-" + (i + 1));
             System.out.println("Kode        : " + arrayOfMatakuliah20[i].kode);
