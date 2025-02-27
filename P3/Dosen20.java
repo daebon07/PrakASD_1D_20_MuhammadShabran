ccad3be34a8757bb4282c27880562a51680361ca
daebon07 <muhammadshabran25@gmail.com>
2025-02-27 14:14:41 +0700

    P3_Tugas1

---
+++ P3/Dosen20.java
@@ -0,0 +1,23 @@
+package P3;
+
+public class Dosen20 {
+    String kode;
+    String nama;
+    Boolean jenisKelamin;
+    int usia;
+
+    public  Dosen20( String kode, String nama, boolean jenisKelamin, int usia) {
+        this.kode = kode;
+        this.nama = nama;
+        this.jenisKelamin = jenisKelamin;
+        this.usia = usia;
+    }
+
+    public void display() {
+        System.out.println("Kode          : " + kode);
+        System.out.println("Nama          : " + nama);
+        System.out.println("Jenis Kelamin : " + (jenisKelamin ? "Pria" : "Wanita"));
+        System.out.println("Usia          : " + usia);
+        System.out.println("------------------------------");
+    }
+}
---
+++ P3/DosenDemo20.java
@@ -0,0 +1,45 @@
+package P3;
+
+import java.util.Scanner;
+
+public class DosenDemo20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah dosen: ");
+        int jumlahDosen = sc.nextInt();
+        sc.nextLine(); 
+
+        Dosen20[] daftarDosen = new Dosen20[jumlahDosen];
+
+        for (int i = 0; i < jumlahDosen; i++) {
+            System.out.println("\nMasukkan Data Dosen ke-" + (i + 1));
+
+            System.out.print("Kode          : ");
+            String kode = sc.nextLine();
+
+            System.out.print("Nama          : ");
+            String nama = sc.nextLine();
+
+            System.out.print("Jenis Kelamin (Pria/Wanita): ");
+            String jk = sc.nextLine();
+            Boolean jenisKelamin = jk.equalsIgnoreCase("Pria");
+
+            System.out.print("Usia          : ");
+            int usia = sc.nextInt();
+            sc.nextLine(); 
+
+            
+            daftarDosen[i] = new Dosen20(kode, nama, jenisKelamin, usia);
+            System.out.println("------------------------------");
+        }
+
+        System.out.println("\nData Dosen yang telah dimasukkan:");
+        for (Dosen20 dosen : daftarDosen) {
+            dosen.display();
+        }
+
+        sc.close();
+    }
+    
+}
