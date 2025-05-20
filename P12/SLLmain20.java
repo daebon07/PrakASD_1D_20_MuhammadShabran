5b90612f778e10494eeaa293066858e19b611c50
daebon07 <muhammadshabran25@gmail.com>
2025-05-20 21:26:29 +0700

    P12_Percobaan 2

--- P12/SLLmain20.java
+++ P12/SLLmain20.java
@@ -1,31 +1,32 @@
 package P12;
 
-import java.util.Scanner;
-
 public class SLLMain20 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        SingleLinkedList20 sll = new SingleLinkedList20();
+        SingleLinkedList20 sll = new SingleLinkedList20()
+;        Mahasiswa20 mhs1 = new Mahasiswa20( "24212200", "Alvaro", "1A", 4.0);
+        Mahasiswa20 mhs2 = new Mahasiswa20("23212201", "Bimon", "2B", 3.8);
+        Mahasiswa20 mhs3 = new Mahasiswa20("22212202", "cintia", "3C",3.5);
+        Mahasiswa20 mhs4 = new Mahasiswa20("21212203","Dirga", "4D", 3.6);
 
-        System.out.print("Masukkan jumlah mahasiswa: ");
-        int jumlah = sc.nextInt(); sc.nextLine();
+        sll.print20();
+        sll.addFirst20(mhs4);
+        sll.print20();
+        sll.addLast20(mhs1);
+        sll.print20();
+        sll.insertAfter20("Dirga", mhs3);
+        sll.insertAt20(2, mhs2);
+        sll.print20();
 
-        for (int i = 0; i < jumlah; i++) {
-            System.out.println("Data Mahasiswa ke-" + (i + 1));
-            System.out.print("NIM: ");
-            String nim = sc.nextLine();
-            System.out.print("Nama: ");
-            String nama = sc.nextLine();
-            System.out.print("Kelas: ");
-            String kelas = sc.nextLine();
-            System.out.print("IPK: ");
-            double ipk = sc.nextDouble(); sc.nextLine();
+        System.out.println("data index 1 : ");
+        sll.getData20(1);
 
-            Mahasiswa20 mhs = new Mahasiswa20(nim, nama, kelas, ipk);
-            sll.addLast20(mhs);
-        }
+        System.out.println("data mahasiswa an Bimon berada pada index : " + sll.indexOf20("bimon"));
+        System.out.println();
 
-        System.out.println("\nData Mahasiswa dalam Linked List:");
-        sll. print20();
-    }
+        sll.removeFirst20();
+        sll.removeLast20();
+        sll.print20();
+        sll.removeAt20(0);
+        sll.print20();
+         }
 }
--- P12/SingleLinkedList20.java
+++ P12/SingleLinkedList20.java
@@ -76,4 +76,87 @@ public class SingleLinkedList20 {
             }
         }
     }
+
+    public void getData20(int index) {
+        NodeMahasiswa20 tmp = head;
+        for (int i = 0; i < index; i++) {
+            tmp = tmp.next;
+        }
+        tmp.data.tampilInformasi20();
+    }
+
+    public int indexOf20(String key) {
+        NodeMahasiswa20 tmp = head;
+        int index = 0;
+        while (tmp != null && !tmp.data.nama.equalsIgnoreCase(key)) {
+            tmp = tmp.next;
+            index++;
+        }
+        if (tmp == null) {
+            return -1;
+        } else {
+            return index;
+        }
+    }
+
+    public void removeFirst20() {
+        if (isEmpty20()) {
+            System.out.println("Linked List masih Kosong, tidak dapat dihapus!");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+        }
+    }
+
+    public void removeLast20() {
+        if (isEmpty20()) {
+            System.out.println("Linked List masih Kosong, tidak dapat dihapus!");
+        } else if (head == tail) {
+            head = tail = null;
+        } else {
+            NodeMahasiswa20 temp = head;
+            while (temp.next != tail) {
+                temp = temp.next;
+            }
+            temp.next = null;
+            tail = temp;
+        }
+    }
+
+    public void remove20(String key) {
+        if (isEmpty20()) {
+            System.out.println("Linked List masih Kosong, tidak dapat dihapus!");
+        } else {
+            NodeMahasiswa20 temp = head;
+            while (temp != null) {
+                if ((temp.data.nama.equalsIgnoreCase(key)) && (temp == head)) {
+                    this.removeFirst20();
+                    break;
+                } else if (temp.data.nama.equalsIgnoreCase(key)) {
+                    temp.next = temp.next.next;
+                    if (temp.next == null) {
+                        tail = temp;
+                    }
+                    break;
+                }
+                temp = temp.next;
+            }
+        }
+    }
+
+    public void removeAt20(int index) {
+        if (index == 0) {
+            removeFirst20();
+        } else {
+            NodeMahasiswa20 temp = head;
+            for (int i = 0; i < index - 1; i++) {
+                temp = temp.next;
+            }
+            temp.next = temp.next.next;
+            if (temp.next == null) {
+                tail = temp;
+            }
+        }
+    }
 }
