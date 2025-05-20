0dfcc8c52130acf24c64f87d6eebf097d509ed08
daebon07 <muhammadshabran25@gmail.com>
2025-05-20 21:05:50 +0700

    P12_Percobaan1_Pertanyaan 3

--- P12/SLLmain20.java
+++ P12/SLLmain20.java
@@ -1,20 +1,31 @@
 package P12;
 
-public class SLLmain20 {
-    public static void main(String[] args) {
-        SingleLinkedList20 sll = new SingleLinkedList20()
-;        Mahasiswa20 mhs1 = new Mahasiswa20( "24212200", "Alvaro", "1A", 4.0);
-        Mahasiswa20 mhs2 = new Mahasiswa20("23212201", "Bimon", "2B", 3.8);
-        Mahasiswa20 mhs3 = new Mahasiswa20("22212202", "cintia", "3C",3.5);
-        Mahasiswa20 mhs4 = new Mahasiswa20("21212203","Dirga", "4D", 3.6);
+import java.util.Scanner;
 
-        sll.print20();
-        sll.addFirst20(mhs4);
-        sll.print20();
-        sll.addLast20(mhs1);
-        sll.print20();
-        sll.insertAfter20("Dirga", mhs3);
-        sll.insertAt20(2, mhs2);
-        sll.print20();
-         }
+public class SLLMain20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        SingleLinkedList20 sll = new SingleLinkedList20();
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlah = sc.nextInt(); sc.nextLine();
+
+        for (int i = 0; i < jumlah; i++) {
+            System.out.println("Data Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM: ");
+            String nim = sc.nextLine();
+            System.out.print("Nama: ");
+            String nama = sc.nextLine();
+            System.out.print("Kelas: ");
+            String kelas = sc.nextLine();
+            System.out.print("IPK: ");
+            double ipk = sc.nextDouble(); sc.nextLine();
+
+            Mahasiswa20 mhs = new Mahasiswa20(nim, nama, kelas, ipk);
+            sll.addLast20(mhs);
+        }
+
+        System.out.println("\nData Mahasiswa dalam Linked List:");
+        sll. print20();
+    }
 }
